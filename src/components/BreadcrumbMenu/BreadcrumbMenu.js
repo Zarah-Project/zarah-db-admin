ec20a8bdd0de0ba704b4fb4e726c48c33c0866b0
József Gábor Bóné <jozsef.bone@gmail.com>
2020-06-09 21:49:17 +0200

    fix(Breadcrumb): Breadcrumb menu correction.

--- src/components/BreadcrumbMenu/BreadcrumbMenu.js
+++ src/components/BreadcrumbMenu/BreadcrumbMenu.js
@@ -1,12 +1,40 @@
 import {Breadcrumb} from "antd";
 import React from "react";
+import {withRouter} from "react-router-dom";
 
-const BreadcrumbMenu = () => {
+const breadcrumbs = {
+  '/documents': ['Documents'],
+  '/documents/view': ['Documents', 'View'],
+  '/documents/edit': ['Documents', 'Edit'],
+  '/documents/create': ['Documents', 'Create'],
+  '/people': ['People'],
+  '/organisations': ['Organisations'],
+  '/places': ['Places']
+};
+
+const BreadcrumbMenu = (props) => {
+  const { location } = props;
+  let path = location.pathname;
+
+  if (path.split('/').length > 2) {
+    path = path.substring(0, path.lastIndexOf('/'))
+  }
+
+  const renderBreadcrumbItem = () => {
+    if (breadcrumbs.hasOwnProperty(path)) {
+      return breadcrumbs[path].map((breadcrumb, idx) => (
+        <Breadcrumb.Item key={idx}>{breadcrumb}</Breadcrumb.Item>
+      ))
+    } else {
+      return ''
+    }
+  };
+
   return (
     <Breadcrumb style={{ margin: '16px 0', padding: '0 24px' }}>
-      <Breadcrumb.Item>Documents</Breadcrumb.Item>
+      {renderBreadcrumbItem()}
     </Breadcrumb>
   )
 };
 
-export default BreadcrumbMenu;
+export default withRouter(BreadcrumbMenu);
