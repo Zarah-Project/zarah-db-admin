ab7c7e6a4569beecb549239d288c01132a6bf554
József Gábor Bóné <jozsef.bone@gmail.com>
2020-09-20 19:56:49 +0200

    fix(validation): Remove date validation

--- src/views/documents/DocumentForm/validation/validation.js
+++ src/views/documents/DocumentForm/validation/validation.js
@@ -3,14 +3,6 @@ import * as yup from 'yup';
 const validation = yup.object().shape({
   title: yup.string()
     .required('This field is required'),
-  dates: yup.array()
-    .of(
-      yup.object()
-        .shape({
-          date_from: yup.string()
-            .required('This field is required'),
-        })
-    )
 });
 
 export default validation;
